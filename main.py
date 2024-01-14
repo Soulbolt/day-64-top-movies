83e0ae3edb85d41983bdb4ba89cf40cbeaf1725a
Soulbolt <soulbolt@gmail.com>
2024-01-14 10:43:38 -0700

    implemented database and schema creation for movie table and model along with one record creation. commented out as they are not needed anymore.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -11,6 +13,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+# Database
+class Base(DeclarativeBase):
+    pass
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my-top-movies.db"
+# Extension
+db = SQLAlchemy(model_class=Base)
+# Initialize the app with the extension
+db.init_app(app)
+# Define Model/Table
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[str] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+# Create the table schema in the database, Requires application context
+# with app.app_context():
+#     db.create_all()
+# Create a Record
+# with app.app_context():
+#     new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg")
+#     db.session.add(new_movie)
+#     db.session.commit()
 
 @app.route("/")
 def home():
